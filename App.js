5648fcd9f77e3c79be030e4127c718d7a87e521e
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2023-09-12 23:07:16 +0530

    Revert "Dev"

--- App.js
+++ App.js
@@ -1,16 +1,51 @@
-import React from "react";
-import ReactDOM  from "react-dom/client";
+/**
+ * <div id="parent">
+ *  <div id="child">
+ *    <h1>I'm h1 tag</h1>
+ *  </div>
+ * </div>
+ */
 
-// This is how we write element in core react 👇
-// React.createElement => JS Object => HTMLElement(render)
-const heading = React.createElement('h1', {id: 'heading'}, 'hello from react!');
+// when we want to create nested element tag then below is the example code
 
-// JSX = HTML like or XML like syntax
-// This is how we write element in react using JSX
-// JSX (JSX code transpiled before it reaches the JS Engine) - Parcel - Babel
+// const parent = React.createElement(
+//   'div', {id: 'parent'}, React.createElement(
+//     'div', {id: 'child'}, React.createElement(
+//       'h1', {}, "I'm a tag")
+//     )
+//   );
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(parent);
 
-//  JSX => React.createElement => JS Object => HTMLElement(render)
-const jsxHeading = <h1 id="heading" >Namaste React using JSX</h1>
+
+// Example for siblings tags
+/**
+ * <div id="parent">
+ *  <div id="child">
+ *    <h1>I'm h1 tag</h1>
+ *    <h2>I'm h1 tag</h2>
+ *  </div>
+ * </div>
+ */
+
+// when we want to create nested element with siblings (eg. h1 and h2 are siblings) tag then below is the example code
+
+const heading1 = React.createElement('h1', {}, "I'm a h1 tag");
+const heading2 = React.createElement('h2', {}, "I'm a h2 tag")
+
+const parent2 = React.createElement(
+  'div', {id: 'parent'}, React.createElement(
+    'div', {id: 'child'}, [heading1, heading2]
+    )
+  );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(jsxHeading);
+root.render(parent2);
+
+// const heading = React.createElement('h1', {id: 'heading'}, 'hello from react!');
+// here React.createElement will create an object and assign it to heading variable
+// below is the result 👇
+// console.log("heading variable",heading)
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(heading);
+// now this render method basically be responsible to take this object (heading obj) and put it up convert it into that heading tag put it up into the DOM which we see in the browser.
