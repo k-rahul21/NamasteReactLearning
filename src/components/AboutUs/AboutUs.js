4d6e547a7a6459b4be4c2a46181b9143d2ad4a87
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2023-09-24 21:05:27 +0530

    create: a class based component

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React from "react";
 
-const AboutUs = () => {
-  return (
-    <div>
-      <h1>About Us Page</h1>
-      <p>Here we'll show the extra details.</p>
-    </div>
-  )
+class AboutUs extends React.Component {
+  // this is how we get the props from parents component.
+  // find out the answer why we use super keyword here.
+  constructor(props) {
+    super(props);
+
+    // this is how we define state in class based component
+    this.state = {
+      count: 0,
+    };
+  }
+  render() {
+    const { Title } = this.props;
+    // if we want to destructure state value then 👇
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>About Us Page {Title}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          {count}
+        </button>
+        <p>Here we'll show the extra details.</p>
+      </div>
+    );
+  }
 }
 
 export default AboutUs;
