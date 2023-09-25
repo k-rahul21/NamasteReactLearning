a23545cdd7dd815d61c57c9e248c57fee75eb763
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2023-09-25 11:35:42 +0530

    show the status of a user in header section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,21 +2,26 @@
 import { Title } from "../Title/Title";
 import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
+import useOnlineStatus from "../../utils/useOnlineStatus";
 import "./Header.scss";
 
 const Header = () => {
+  const onlineStatus = useOnlineStatus();
   return (
     <div className="header">
       <Title />
       {/* <SearchBar/> */}
       <div className="nav-items">
         <ul>
-          <Link to="/about">
+          <Link>
+            <li>Online Status : {onlineStatus ? "✅" : "❌"}</li>
+          </Link>
+          {/* <Link to="/about">
             <li>About</li>
-          </Link>
-          <Link to="/contact">
+          </Link> */}
+          {/* <Link to="/contact">
             <li>Contact</li>
-          </Link>
+          </Link> */}
           <Link to="/cart">
             <li>Cart</li>
           </Link>
