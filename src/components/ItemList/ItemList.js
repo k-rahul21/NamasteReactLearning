50982625b0322b4d7f7f820ec9b38afb392a81bd
Khandelwal <830474@sita.in>
2023-11-13 17:50:11 +0530

    add: add to cart btn

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -8,21 +8,29 @@ const ItemList = ({ item }) => {
           key={item.card.info.id}
           className="border-b-2 my-2 py-2 flex justify-between"
         >
-          <div className="flex flex-col">
+          <div className="flex flex-col w-9/12">
             <div className="pr-2">{item.card.info.name}</div>
             <div>
               {" "}
               ₹{" "}
               {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
             </div>
-            <div className="text-custom-light-gray w-9/12 mt-2">
+            <div
+              className={`text-custom-light-gray mt-2 
+               `}
+            >
               {item.card.info.description}.
             </div>
           </div>
-          <img
-            src={IMG_CDN_URL + item.card.info?.imageId}
-            className="h-24 w-28 rounded-lg"
-          />
+          <div>
+            <img
+              src={IMG_CDN_URL + item.card.info?.imageId}
+              className="h-24 w-28 rounded-lg"
+            />
+            <button className="border-2 border-custom-black py-1 px-4 relative left-5 bottom-5 bg-white">
+              ADD
+            </button>
+          </div>
         </div>
       ))}
     </div>
