8e2ff1acd4ebecb0466f8dd4ffc8c9cbdd49b255
Khandelwal <830474@sita.in>
2023-11-20 21:48:30 +0530

    make: cart item card

--- src/components/CartDetails/CartItem.js
+++ src/components/CartDetails/CartItem.js
@@ -1,14 +1,19 @@
 import React from "react";
 import AvailableOffer from "../AvailableOffer/AvailableOffer";
+import CartItemCard from "./CartItemCard";
 
 const CartItem = ({ data }) => {
-  console.log("cartItem", data);
   return (
-    <div className="w-6/12 border-2 border-custom-light-gray cart-item-container p-3">
+    <div className="w-6/12 cart-item-container p-3 border-r-2">
       <AvailableOffer />
-      <div className="flex justify-between m-2 px-8">
+      <div className="flex justify-between my-2 px-8">
         <div className="font-bold text-lg">{data.length} ITEMS ADDED</div>
         <button className="font-bold">REMOVE</button>
+      </div>
+      <div className="overflow-y-scroll h-96 px-2">
+        {data.map((dataa) => (
+          <CartItemCard cartItem={dataa} />
+        ))}
       </div>
     </div>
   );
---
+++ src/components/CartDetails/CartItemCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { IMG_CDN_URL } from "../../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeItems } from "../../utils/cartSlice";
+import { X } from "lucide-react";
+
+const CartItemCard = ({ cartItem }) => {
+  const dispatch = useDispatch();
+  const handleRemoveItemFromCart = (item) => {
+    dispatch(removeItems(item));
+  };
+  return (
+    <div className="my-2 pl-8 pr-3 py-3 border-2 border-custom-light-gray rounded flex flex-row justify-between">
+      <div className="flex flex-col gap-1">
+        <div>{cartItem?.name}</div>
+        <div className="text-sm text-custom-dark-gray">
+          Category: {cartItem?.category}
+        </div>
+        <div className="text-sm text-custom-dark-gray">QTY: 1</div>
+        <div className="flex flex-row gap-2">
+          <div className="price">₹ {cartItem?.price / 100}</div>{" "}
+          {cartItem?.defaultPrice && (
+            <div className="default-price text-custom-dark-gray line-through">
+              ₹ {cartItem?.defaultPrice / 100}
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="flex flex-row gap-2">
+        <img
+          src={IMG_CDN_URL + cartItem.imageId}
+          className="h-24 w-28 rounded-lg"
+        />
+        <X
+          className="cursor-pointer"
+          onClick={() => handleRemoveItemFromCart(cartItem)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CartItemCard;
