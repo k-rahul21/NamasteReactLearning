88e9a771d6679f015b3b5a07e1b28fb52ad93098
Khandelwal <830474@sita.in>
2023-11-21 16:44:36 +0530

    style: rm border

--- src/components/CartDetails/CartTotal.js
+++ src/components/CartDetails/CartTotal.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const CartTotal = ({ data }) => {
   return (
-    <div className="w-6/12 border-2 border-l-0 border-custom-light-gray cart-total-container p-3">
+    <div className="w-6/12 cart-total-container p-3">
       <h1>Cart total comes here</h1>{" "}
     </div>
   );
