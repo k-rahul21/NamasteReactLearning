2f163e1bd29c6b263f849060189031f08bdc0990
Khandelwal <830474@sita.in>
2023-12-13 07:43:05 +0530

    rm: path for about us and contact us page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Body from "./components/Body/Body";
-import AboutUs from "./components/AboutUs/AboutUs";
 import ErrorPage from "./components/ErrorPage/ErrorPage";
-import ContactUs from "./components/ContactUs/ContactUs";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Cart from "./components/Cart/Cart";
 import RestrauntMenu from "./components/RestrauntMenu/RestrauntMenu";
@@ -33,14 +31,6 @@ const appRoute = createBrowserRouter([
       {
         path: "/",
         element: <Body />,
-      },
-      {
-        path: "/about",
-        element: <AboutUs Title={"Class Based Component"} />,
-      },
-      {
-        path: "/contact",
-        element: <ContactUs />,
       },
       {
         path: "/cart",
